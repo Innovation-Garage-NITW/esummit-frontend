cfa4346f41f3a2e0603d70c9d4f681dbe251e139
Jytesh <44925963+Jytesh@users.noreply.github.com>
2024-01-17 09:36:50 +0530

    Add to landing page
    Its broken
    
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- src/MyComponents/Pages/Home.jsx
+++ src/MyComponents/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import {useEffect} from "react";
 import { gsap } from "gsap";
 import "./Home.css";
 import bgImg from "../Images/new-bg.png"
@@ -14,13 +14,16 @@ import mountain6Left from "../Images/mountain-6-left.png"
 import waveLeft from "../Images/wave-left.png"
 import logo from "../Images/logo.png"
 import tree from "../Images/Trees.png"
-import fade from "../Images/groupmountain.png"
 import mountain5Right from "../Images/Vector70.png"
 import mountain7Left from "../Images/Vector74.png"
 import overview from "../Images/overview.png"
 import overviewtext from "../Images/overviewtext.png"
 import overviewbox from "../Images/overviewbox.png"
 import text from "../Images/text.png"
+import { Container } from "react-bootstrap";
+import EventCard from './EventsComp/EventCard'
+import posterUrl from "../../assets/poster_innovate_sphere.jpeg";
+
 //import OverviewSection from "./OverviewSection";
 
 export const Home = () => {
@@ -104,6 +107,40 @@ export const Home = () => {
     }
   }, [])
   
+
+const events = [
+  {
+    image: posterUrl,
+    title: "Innovate Sphere",
+    details: "lot of details",
+  },
+  {
+    image: posterUrl,
+    title: "Innovate Sphere",
+    details: "lot of details",
+  },
+  {
+    image: posterUrl,
+    title: "Innovate Sphere",
+    details: "lot of details",
+  },
+  {
+    image: posterUrl,
+    title: "Innovate Sphere",
+    details: "lot of details",
+  },
+  {
+    image: posterUrl,
+    title: "Innovate Sphere",
+    details: "lot of details",
+  },
+  {
+    image: posterUrl,
+    title: "Innovate Sphere",
+    details: "lot of details",
+  },
+]
+  
   return (
     <>
     <div className="main">
@@ -122,8 +159,8 @@ export const Home = () => {
       <img src={overview} alt="" className="overview"/>
       <img src={overviewbox} alt="" className="overviewbox" />
       <img src={overviewtext} alt="" className="overviewtext" />
-      <img src={mountain5Right} alr="" className="mountain-5-right"/>
-      <img src={mountain7Left} alr="" className="mountain-7-left"/>
+      <img src={mountain5Right} alt="" className="mountain-5-right"/>
+      <img src={mountain7Left} alt="" className="mountain-7-left"/>
       <img src={text} alt="" className="text" />
 
       {/* Trees  */}
@@ -131,7 +168,22 @@ export const Home = () => {
       <img src={tree} alt="" data-speedx="0.02" data-speedy="0.05" data-speedz="0.2" data-distance="800" className="tree"/>
       <img src={tree} alt="" data-speedx="0.02" data-speedy="0.05" data-speedz="0.2" data-distance="800" className="tree1"/>
 
-      <p className="para">Crafted for the seekers of inspiration:<br></br> ESummit beckons. Dive into a realm where<br></br> sparks of innovation meet the whispers of <br></br> change. Join us in shaping a future beyond <br></br> boundaries at NITW premeier event. <br></br> ESummit--where creativity takes flight <br></br> in the uncharted</p>
+        <p className="para">Crafted for the seekers of inspiration:<br></br> ESummit beckons. Dive into a realm where<br></br> sparks of innovation meet the whispers of <br></br> change. Join us in shaping a future beyond <br></br> boundaries at NITW premeier event. <br></br> ESummit--where creativity takes flight <br></br> in the uncharted</p>
+        <div className="belowPara">
+          <Container>
+            {
+              events.map((event, index) => (
+                <EventCard
+                  key={index}
+                  image={event.image}
+                  title={event.title}
+                  details={event.details}
+                />
+              ))
+          }
+          
+          </Container>
+        </div>
     </div>
     
     </>
