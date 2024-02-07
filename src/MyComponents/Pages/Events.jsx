c706e7785439f1df390f951f219a8b215c54136c
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2024-02-08 00:11:29 +0530

    fetching backend data

--- src/MyComponents/Pages/Events.jsx
+++ src/MyComponents/Pages/Events.jsx
@@ -4,20 +4,34 @@ import ImageCard from './EventsComp/ImageCard'
 import posterUrl from "../../assets/poster_innovate_sphere.jpeg";
 import { Container } from 'react-bootstrap';
 import { EventsData } from '../../../data';
+import { useEffect, useState } from 'react';
+import { getEvents } from '../../../backend_functions';
 
 export const Events = () => {
+
+	const [eventsData, setEventsData] = useState([]);
+
+	useEffect(() => {
+		// setEventsData(EventsData);
+		async function fetchData() {
+			const data = await getEvents();
+			setEventsData(data);
+		}
+		fetchData();
+	}, [])
+
 	return (
 		<>
 			<Banner heading="EVENTS" imgUrl={imgUrl} />
 			<div className="Content" style={{}}>
 				<Container style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop: '5%' }}>
 					{
-						EventsData.map((event, index) => (
+						eventsData.map((event, index) => (
 							<ImageCard
 								key={index}
-								image={event.image}
-								title={event.title}
-								details={event.details}
+								image={event['photo']}
+								title={event['name']}
+								details={event['description']}
 								sizing={350}
 							/>
 						))
--- src/MyComponents/Pages/Sponsors.jsx
+++ src/MyComponents/Pages/Sponsors.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { getSponsors } from '../../../backend_functions'
 
 export const Sponsors = () => {
+
+  useEffect(() => {
+    getSponsors();
+  })
+
   return (
     <div>
       Sponsors
