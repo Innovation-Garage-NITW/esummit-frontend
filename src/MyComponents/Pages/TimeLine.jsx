068f2a45d09437fd1fdf63a5754aba847c5a4968
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2024-02-16 19:34:09 +0530

    timeline done

--- src/MyComponents/Pages/TimeLine.jsx
+++ src/MyComponents/Pages/TimeLine.jsx
@@ -6,125 +6,13 @@ import { getEvents } from '../../../backend_functions';
 
 function TimeLine() {
 
-    const [day, setDay] = useState(0);
+    const [day, setDay] = useState(1);
     const [timeLineData, setTimeLineData] = useState([]);
+    const [gotData, setGotData] = useState(false);
 
+    const [dummyData, setDummyData] = useState([]);
 
-    const [dummyData, setDummyData] = useState([
-        {
-            title: "Title 1",
-            priority: 1,
-            shortdes: "something",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis orci sed nisi hendrerit condimentum. Fusce ultricies velit ut ultrices auctor. Donec dictum ante nec neque blandit, et maximus massa tempus. Mauris eros velit, scelerisque nec tortor id, faucibus posuere lorem. Cras vel urna a magna euismod vulputate a ac eros. Morbi rutrum faucibus justo, suscipit aliquam dui commodo in. Mauris ut magna ac risus scelerisque vulputate sit amet nec orci.",
-            day: 1,
-            venue: "ALC",
-            time: "16th March 2024 , 5:00 PM",
-            category: 'Competition',
-            imagUrl: 'https://unsplash.com/photos/man-in-black-crew-neck-t-shirt-using-black-laptop-computer-b9-odQi5oDo',
-            unstopLink: "",
-            prizes: "",
-        },
-        {
-            title: "Title 1-Not imp",
-            priority: 2,
-            shortdes: "something",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis orci sed nisi hendrerit condimentum. Fusce ultricies velit ut ultrices auctor. Donec dictum ante nec neque blandit, et maximus massa tempus. Mauris eros velit, scelerisque nec tortor id, faucibus posuere lorem. Cras vel urna a magna euismod vulputate a ac eros. Morbi rutrum faucibus justo, suscipit aliquam dui commodo in. Mauris ut magna ac risus scelerisque vulputate sit amet nec orci.",
-            day: 1,
-            venue: "ALC",
-            time: "5:00 PM",
-            category: 'Competition',
-            imagUrl: 'https://unsplash.com/photos/man-in-black-crew-neck-t-shirt-using-black-laptop-computer-b9-odQi5oDo'
-        },
-        {
-            title: "Title 2",
-            priority: 1,
-            shortdes: "something",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis orci sed nisi hendrerit condimentum. Fusce ultricies velit ut ultrices auctor. Donec dictum ante nec neque blandit, et maximus massa tempus. Mauris eros velit, scelerisque nec tortor id, faucibus posuere lorem. Cras vel urna a magna euismod vulputate a ac eros. Morbi rutrum faucibus justo, suscipit aliquam dui commodo in. Mauris ut magna ac risus scelerisque vulputate sit amet nec orci.",
-            day: 2,
-            venue: "ALC",
-            time: "5:00 PM",
-            category: 'Competition',
-            imagUrl: 'https://unsplash.com/photos/man-in-black-crew-neck-t-shirt-using-black-laptop-computer-b9-odQi5oDo'
-        },
-        {
-            title: "Title 1",
-            priority: 1,
-            shortdes: "something",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis orci sed nisi hendrerit condimentum. Fusce ultricies velit ut ultrices auctor. Donec dictum ante nec neque blandit, et maximus massa tempus. Mauris eros velit, scelerisque nec tortor id, faucibus posuere lorem. Cras vel urna a magna euismod vulputate a ac eros. Morbi rutrum faucibus justo, suscipit aliquam dui commodo in. Mauris ut magna ac risus scelerisque vulputate sit amet nec orci.",
-            day: 2,
-            venue: "ALC",
-            time: "5:00 PM",
-            category: 'Competition',
-            imagUrl: 'https://unsplash.com/photos/man-in-black-crew-neck-t-shirt-using-black-laptop-computer-b9-odQi5oDo'
-        },
-        {
-            title: "Title 1",
-            priority: 1,
-            shortdes: "something",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis orci sed nisi hendrerit condimentum. Fusce ultricies velit ut ultrices auctor. Donec dictum ante nec neque blandit, et maximus massa tempus. Mauris eros velit, scelerisque nec tortor id, faucibus posuere lorem. Cras vel urna a magna euismod vulputate a ac eros. Morbi rutrum faucibus justo, suscipit aliquam dui commodo in. Mauris ut magna ac risus scelerisque vulputate sit amet nec orci.",
-            day: 1,
-            venue: "ALC",
-            time: "5:00 PM",
-            category: 'Competition',
-            imagUrl: 'https://unsplash.com/photos/man-in-black-crew-neck-t-shirt-using-black-laptop-computer-b9-odQi5oDo'
-        },
-        {
-            title: "Title 1",
-            priority: 1,
-            shortdes: "something",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis orci sed nisi hendrerit condimentum. Fusce ultricies velit ut ultrices auctor. Donec dictum ante nec neque blandit, et maximus massa tempus. Mauris eros velit, scelerisque nec tortor id, faucibus posuere lorem. Cras vel urna a magna euismod vulputate a ac eros. Morbi rutrum faucibus justo, suscipit aliquam dui commodo in. Mauris ut magna ac risus scelerisque vulputate sit amet nec orci.",
-            day: 1,
-            venue: "ALC",
-            time: "5:00 PM",
-            category: 'Competition',
-            imagUrl: 'https://unsplash.com/photos/man-in-black-crew-neck-t-shirt-using-black-laptop-computer-b9-odQi5oDo'
-        },
-        {
-            title: "Title 1",
-            priority: 1,
-            shortdes: "something",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis orci sed nisi hendrerit condimentum. Fusce ultricies velit ut ultrices auctor. Donec dictum ante nec neque blandit, et maximus massa tempus. Mauris eros velit, scelerisque nec tortor id, faucibus posuere lorem. Cras vel urna a magna euismod vulputate a ac eros. Morbi rutrum faucibus justo, suscipit aliquam dui commodo in. Mauris ut magna ac risus scelerisque vulputate sit amet nec orci.",
-            day: 2,
-            venue: "ALC",
-            time: "5:00 PM",
-            category: 'Competition',
-            imagUrl: 'https://unsplash.com/photos/man-in-black-crew-neck-t-shirt-using-black-laptop-computer-b9-odQi5oDo'
-        },
-        {
-            title: "Title 1",
-            priority: 1,
-            shortdes: "something",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis orci sed nisi hendrerit condimentum. Fusce ultricies velit ut ultrices auctor. Donec dictum ante nec neque blandit, et maximus massa tempus. Mauris eros velit, scelerisque nec tortor id, faucibus posuere lorem. Cras vel urna a magna euismod vulputate a ac eros. Morbi rutrum faucibus justo, suscipit aliquam dui commodo in. Mauris ut magna ac risus scelerisque vulputate sit amet nec orci.",
-            day: 1,
-            venue: "ALC",
-            time: "5:00 PM",
-            category: 'Competition',
-            imagUrl: 'https://unsplash.com/photos/man-in-black-crew-neck-t-shirt-using-black-laptop-computer-b9-odQi5oDo'
-        },
-        {
-            title: "Title 1",
-            priority: 1,
-            shortdes: "something",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis orci sed nisi hendrerit condimentum. Fusce ultricies velit ut ultrices auctor. Donec dictum ante nec neque blandit, et maximus massa tempus. Mauris eros velit, scelerisque nec tortor id, faucibus posuere lorem. Cras vel urna a magna euismod vulputate a ac eros. Morbi rutrum faucibus justo, suscipit aliquam dui commodo in. Mauris ut magna ac risus scelerisque vulputate sit amet nec orci.",
-            day: 2,
-            venue: "ALC",
-            time: "5:00 PM",
-            category: 'Competition',
-            imagUrl: 'https://unsplash.com/photos/man-in-black-crew-neck-t-shirt-using-black-laptop-computer-b9-odQi5oDo'
-        },
-        {
-            title: "Title 1",
-            priority: 1,
-            shortdes: "something",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis orci sed nisi hendrerit condimentum. Fusce ultricies velit ut ultrices auctor. Donec dictum ante nec neque blandit, et maximus massa tempus. Mauris eros velit, scelerisque nec tortor id, faucibus posuere lorem. Cras vel urna a magna euismod vulputate a ac eros. Morbi rutrum faucibus justo, suscipit aliquam dui commodo in. Mauris ut magna ac risus scelerisque vulputate sit amet nec orci.",
-            day: 1,
-            venue: "ALC",
-            time: "5:00 PM",
-            category: 'Competition',
-            imagUrl: 'https://unsplash.com/photos/man-in-black-crew-neck-t-shirt-using-black-laptop-computer-b9-odQi5oDo'
-        },
-    ]);
-
+    // const url = 'https://learn.trybooking.com/hc/article_attachments/360004770073/Booking_URL_Information_Page.png';
 
     useEffect(() => {
         async function fetchData() {
@@ -136,42 +24,21 @@ function TimeLine() {
                 console.log("Error fetching the timeline events ", error);
             }
         }
-        fetchData();
-        let temp = [];
-        for (var i = 0; i < dummyData.length; i++) {
-            if (dummyData[i].priority === 1 && dummyData[i].day === day) {
-                var t = {
-                    title: dummyData[i].time,
-                    cardTitle: dummyData[i].title,
-                    cardSubtitle: dummyData[i].shortdes,
-                    cardDetailedText: dummyData[i].description,
-                    media: {
-                        type: "IMAGE",
-                        source: {
-                            url: dummyData[i].imagUrl,
-                        }
-                    }
-                }
-                temp.push(t);
-            }
+        fetchData().then(() => {
+            setGotData(true);
         }
-        setTimeLineData(temp);
+        )
+
     }, []);
 
     useEffect(() => {
-        async function fetchData() {
-            try {
-                const data = await getEvents();
-                setDummyData(data);
-            }
-            catch (error) {
-                console.log("Error fetching the timeline events ", error);
-            }
-        }
-        fetchData();
+        updateEvents();
+    }, [day, gotData]);
+
+    const updateEvents = () => {
         let temp = [];
         for (var i = 0; i < dummyData.length; i++) {
-            if (dummyData[i].priority === 1 && dummyData[i].day === day) {
+            if (dummyData[i].priority == '1' && dummyData[i].day == day.toString()) {
                 var t = {
                     title: dummyData[i].time,
                     cardTitle: dummyData[i].title,
@@ -180,7 +47,7 @@ function TimeLine() {
                     media: {
                         type: "IMAGE",
                         source: {
-                            url: dummyData[i].imagUrl,
+                            url: dummyData[i].imageUrl,
                         }
                     }
                 }
@@ -188,7 +55,7 @@ function TimeLine() {
             }
         }
         setTimeLineData(temp);
-    }, [day]);
+    }
 
 
     return (
