2c0d392d0e63064424987928b7173ccfd7e6a1e4
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2024-03-06 18:18:36 +0530

    added spinner and removed strict mode

--- src/MyComponents/Spinner.jsx
+++ src/MyComponents/Spinner.jsx
@@ -2,6 +2,6 @@ import { SpinnerCircular } from "spinners-react"
 
 export default function Spinner() {
     return (
-        <SpinnerCircular size={66} thickness={100} speed={100} color="rgba(68, 57, 172, 1)" secondaryColor="rgba(0, 0, 0, 0.44)" />
+        <SpinnerCircular size={66} thickness={100} speed={100} color="#ffffff" secondaryColor="rgba(0, 0, 0, 0.44)" />
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,5 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </BrowserRouter>,
   {/* </React.StrictMode> */ }
-
+  ,
 )
