225b5ab323d96d0d58e43a08aeb358273eee8331
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2024-03-11 22:13:29 +0530

    Minor changes

--- backend_functions.js
+++ backend_functions.js
@@ -86,6 +86,7 @@ export async function getTeams() {
 export async function registerUser(user, name, email, college) {
     try {
         const idToken = await getIdToken(user);
+        console.log(idToken);
         const response = await fetch('https://us-central1-esummit-ig.cloudfunctions.net/register', {
             method: 'POST',
             headers: {
@@ -102,7 +103,7 @@ export async function registerUser(user, name, email, college) {
             })
         });
         const data = await response.json();
-        // console.log(data);
+        console.log(data);
         return data;
     } catch (error) {
         console.log("Error registering user:", error);
@@ -112,6 +113,7 @@ export async function registerUser(user, name, email, college) {
 export async function loginUser(user) {
     try {
         const idToken = await getIdToken(user);
+        console.log(idToken);
         const response = await fetch('https://us-central1-esummit-ig.cloudfunctions.net/login', {
             method: 'POST',
             headers: {
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  {/* </React.StrictMode> */ }
-  ,
+  </BrowserRouter>
 )
